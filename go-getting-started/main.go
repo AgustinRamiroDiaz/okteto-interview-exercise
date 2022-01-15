4e28c70d927b3f0fa3a435a267f414f2ae717bd3
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2022-01-15 13:30:32 -0300

    Pod count implementation with client-go

--- go-getting-started/main.go
+++ go-getting-started/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
 	fmt.Println("Starting hello-world server...")
 	http.HandleFunc("/", helloServer)
+	http.HandleFunc("/pods", podsInCurrentNamespace)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
@@ -15,4 +21,22 @@ func main() {
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello Oktetos!")
+}
+
+func podsInCurrentNamespace(w http.ResponseWriter, r *http.Request) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	// get pods in all the namespaces by omitting namespace
+	// Or specify namespace to get pods in particular namespace
+	pods, err := clientset.CoreV1().Pods("agustinramirodiaz").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprint(w, "There are ", len(pods.Items), " pods in the cluster\n")
 }
